288557b38baf09fb1f375435c97646c9aeadc2a2
ElenaMatzuk <e.m.ratnikova@gmail.com>
2020-09-07 12:12:50 +0300

    hw8 finish

---
+++ hw7/src/public/js/ProductComponent.js
@@ -0,0 +1,52 @@
+const products = {
+   data(){
+       return {
+           catalogUrl: '/catalogData.json',
+           filtered: [],
+           products: [],
+           imgProduct: 'https://placehold.it/200x150'
+       }
+   },
+    components: {
+        product
+    },
+    mounted(){
+        this.$parent.getJson(`/api/products`)
+            .then(data => {
+                for (let item of data){
+                    this.$data.products.push(item);
+                    this.$data.filtered.push(item);
+                }
+            });
+    },
+    methods: {
+        filter(userSearch){
+            let regexp = new RegExp(userSearch, 'i');
+            this.filtered = this.products.filter(el => regexp.test(el.product_name));
+        }
+    },
+   template: `<div class="products">
+                <product v-for="item of filtered" 
+                :key="item.id_product" 
+                :img="item.image"
+                :product="item"
+                @add-product="$parent.$refs.cart.addProduct"></product>
+               </div>`
+};
+const product = {
+    props: ['product', 'img'],
+    template: `
+            <div class="product-item">
+            <img :src="img" alt="Some img" >
+                <div class="desc">
+                    <h3>{{product.product_name}}</h3>
+                    <div class="price">
+                        <p>{{product.price}} рублей</p>
+                        <button class="buy-btn" @click="$emit('add-product', product)">Купить</button>
+                    </div>
+                </div>
+            </div>
+    `
+};
+
+export default products
